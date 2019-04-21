fd2fab9f149dfe48a446e0048b8e5c17a7751034
philiplambok <philiplambok71@gmail.com>
2019-04-21 20:10:18 +0700

    add music model and seeds that.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+40.times do
+  title = Faker::Coffee.blend_name
+  author = Faker::Name.name
+  url = Faker::Avatar.image
+  Music.create!(
+    title: title,
+    author: author,
+    download_url: url
+  )
+end
